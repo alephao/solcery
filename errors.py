f279fe7c6576dad6bda46f09a2b2216cbf6e6c54
alephao <7674479+alephao@users.noreply.github.com>
2022-03-08 14:38:16 -0300

    feat: add solhint disable func-name-mixedcase

--- errors.py
+++ errors.py
@@ -2,7 +2,7 @@ template = """// SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.4;
 
 // The code in this file was generate. Do not modify!
-// solhint-disable const-name-snakecase
+// solhint-disable const-name-snakecase,func-name-mixedcase
 library Errors {
 <CONTENT>
 }
